2f7f6bb1f987e0d7de31cac853b21bb6532486d4
mrsajadpp <mrsajadpp@gmail.com>
2025-02-02 19:52:43 +0530

    first commit

---
+++ api/index.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3009;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+export default app;
